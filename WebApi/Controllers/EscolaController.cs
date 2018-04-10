aed616ad9a615838ac723bc4f03cee78f26cda50
Felipe Guimarães <fguimaraes@granadophebo.com.br>
2018-04-10 17:50:09 -0300

    Sobe arquivos

---
+++ WebApi/Controllers/EscolaController.cs
@@ -0,0 +1,102 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Web.Http.Cors;
+using Newtonsoft.Json;
+
+namespace WebApi.Controllers
+{
+    public class EscolaController : ApiController
+    {
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IEnumerable<Escola> Get()
+        {
+            using (elevaEntities DAL = new elevaEntities())
+            {
+                IEnumerable<Escola> Escola = DAL.Escola.Include("Turma").ToList();
+                return Escola;
+            }
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public Escola Get(int id)
+        {
+            using(var DAL = new elevaEntities())
+            {
+                Escola Model = DAL.Escola.Where(x => x.ID == id).Include("Turma").FirstOrDefault();
+
+                return Model;
+            }
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*"), HttpPost]
+        public IHttpActionResult Create([FromBody] Escola escola)
+        {
+            //var Model = JsonConvert.DeserializeObject<Escola>(escola);
+            if (escola == null)
+            {
+                return BadRequest();
+            }
+            using (var DAL = new elevaEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    DAL.Entry(escola).State = EntityState.Added;
+
+                    DAL.SaveChanges();
+                }
+            }
+            return Ok();
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*"), HttpPut]
+        public IHttpActionResult Update(long id, [FromBody] Escola escola)
+        {
+            if (escola == null || escola.ID != id)
+            {
+                return BadRequest();
+            }
+
+            using (var DAL = new elevaEntities())
+            {
+                var Model = DAL.Escola.Where(x => x.ID == id).FirstOrDefault();
+
+                if (Model == null)
+                {
+                    return NotFound();
+                }
+
+                Model.Nome = escola.Nome;
+                Model.Endereco = escola.Endereco;
+
+                DAL.Entry(Model).State = EntityState.Modified;
+
+                DAL.SaveChanges();
+            }
+            return Ok();
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*"), HttpDelete]
+        public IHttpActionResult Delete(long id)
+        {
+            using (var DAL = new elevaEntities())
+            {
+                var Model = DAL.Escola.Where(x => x.ID == id).FirstOrDefault();
+                if(Model == null)
+                {
+                    return NotFound();
+                }
+
+                DAL.Entry(Model).State = EntityState.Deleted;
+                DAL.SaveChanges();
+                return Ok();
+            }
+        }
+
+    }
+}
---
+++ WebApi/Controllers/TurmaController.cs
@@ -0,0 +1,106 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Web.Http.Cors;
+
+namespace WebApi.Controllers
+{
+    public class TurmaController : ApiController
+    {
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IEnumerable<Turma> Get()
+        {
+            using (elevaEntities DAL = new elevaEntities())
+            {
+                IEnumerable<Turma> Turma = DAL.Turma.Include("Escola").ToList();
+                return Turma;
+            }
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public Turma Get(int id)
+        {
+            using (var DAL = new elevaEntities())
+            {
+                Turma Model = DAL.Turma.Where(x => x.ID == id).FirstOrDefault();
+
+                return Model;
+            }
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*"), HttpPost]
+        public IHttpActionResult Create([FromBody] Turma turma)
+        {
+            if (turma == null)
+            {
+                return BadRequest();
+            }
+            using (var DAL = new elevaEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    var Escola = DAL.Escola.Where(x=>x.ID == turma.IDEscola).FirstOrDefault();
+                    if (Escola == null)
+                        return BadRequest();
+                    DAL.Entry(turma).State = EntityState.Added;
+
+                    DAL.SaveChanges();
+                }
+            }
+            return Ok();
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*"), HttpPut]
+        public IHttpActionResult Update(long id, [FromBody] Turma turma)
+        {
+            if (turma == null || turma.ID != id)
+            {
+                return BadRequest();
+            }
+
+            using (var DAL = new elevaEntities())
+            {
+                var Model = DAL.Turma.Where(x => x.ID == id).FirstOrDefault();
+
+                if (Model == null)
+                {
+                    return NotFound();
+                }
+
+                var Escola = DAL.Escola.Where(x => x.ID == turma.IDEscola).FirstOrDefault();
+                if (Escola == null)
+                    return BadRequest();
+
+                Model.Nome = turma.Nome;
+                Model.IDEscola = turma.IDEscola;
+
+                DAL.Entry(Model).State = EntityState.Modified;
+
+                DAL.SaveChanges();
+            }
+            return Ok();
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*"), HttpDelete]
+        public IHttpActionResult Delete(long id)
+        {
+            using (var DAL = new elevaEntities())
+            {
+                var Model = DAL.Turma.Where(x => x.ID == id).FirstOrDefault();
+                if (Model == null)
+                {
+                    return NotFound();
+                }
+
+                DAL.Entry(Model).State = EntityState.Deleted;
+                DAL.SaveChanges();
+                return Ok();
+            }
+        }
+    }
+}
